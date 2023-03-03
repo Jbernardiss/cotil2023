c95fffe19ec7e10af9b95dc9a76db60a50025d7d
Jbernardiss <joaobernardiss@gmail.com>
2023-03-02 22:43:09 -0300

    Finished this hellish exercise. The erros were a wrong matrix comparison inside the inset double for and an if statement inside matrixProfile.numProfileExists.

--- DAD/01.03/MatrixProfiler/MatrixProfiler/Main.cs
+++ DAD/01.03/MatrixProfiler/MatrixProfiler/Main.cs
@@ -31,47 +31,105 @@ namespace MatrixProfiler
 	class matrixProfile
 	{
 		private numProfile[] numProfiles = new numProfile[16];
-		private int lastNumProfilePos = 0;
+		private int lastNumProfilePos = -1;
+
+		public matrixProfile()
+		{
+			for(int i = 0; i < 16; i++)
+			{
+				numProfiles[i] = new numProfile();
+				numProfiles[i].setNum(0);
+			}
+		}
 
 		public void addNumProfile(int num, int appearances)
 		{
-			numProfiles[lastNumProfilePos].setNum(num);
+            lastNumProfilePos++;
+            numProfiles[lastNumProfilePos].setNum(num);
 			numProfiles[lastNumProfilePos].setApperances(appearances);
-			lastNumProfilePos++;
 		}
 
 		public void printNumProfiles()
 		{
-			for(int i = 0; i < lastNumProfilePos; i++)
+			for(int i = 0; i <= lastNumProfilePos; i++)
 			{
-				Console.WriteLine("Num: " + numProfiles[i].getNum() + " Appear.: " + numProfiles[i].getApperances());
+				Console.WriteLine("Num: " + numProfiles[i].getNum() + " Appears.: " + numProfiles[i].getApperances());
 			}
+		}
+
+		public bool numProfileExists(int num)
+		{
+			for (int i = 0; i <= lastNumProfilePos; i++)
+			{
+				if (numProfiles[i].getNum() == num)
+				{
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 
 
 	public class Program
 	{
-
 		static void Main(string[] args)
 		{
             matrixProfile matrixProfile = new matrixProfile();
             int[,] matrix = new int[4, 4];
 			int maxNum;
 			int minNum;
-
-			matrixProfile.printNumProfiles();
 			
 			for(int i = 0; i < 4; i++)
 			{
-				for(int j = 0; j < 0; j++)
+				for(int j = 0; j < 4; j++)
 				{
 					Console.Write("[" + i + "," + j + "]: ");
-					matrix[i,j] =  Convert.ToInt32(Console.ReadLine());
+					matrix[i,j] = Convert.ToInt32(Console.ReadLine());
 				}
 			}
 
-		}
+			maxNum = matrix[0, 0];
+			minNum = matrix[0, 0];
+			
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+					if (matrix[i,j] > maxNum)
+					{
+						maxNum = matrix[i,j];
+					}
+					if (matrix[i,j] < minNum)
+					{
+						minNum = matrix[i,j];
+					}
+
+                    if (matrixProfile.numProfileExists(matrix[i,j])) 
+                    {
+						continue;
+                    } else
+					{
+						int counter = 0;
+                        for (int k = 0; k < 4; k++)
+                        {
+                            for (int l = 0; l < 4; l++)
+                            {
+								if (matrix[i,j] == matrix[k,l])
+								{
+									counter++;
+								}
+                            }
+                        }
+						matrixProfile.addNumProfile(matrix[i,j], counter);
+                    }
+                }
+            }
+
+			matrixProfile.printNumProfiles();
+			Console.WriteLine("Max num: " + maxNum);
+			Console.WriteLine("Min num: " + minNum);
+        }
 	}
 
 
