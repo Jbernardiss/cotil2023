e2fbcd98713edbec82fdd2812b4c2f062bea4e4d
Jbernardiss <joaobernardiss@gmail.com>
2023-03-08 15:20:10 -0300

    bruh

---
+++ DAD/08.03/bubblesort/ordenacaoAlgoritmo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ordenacaoAlgoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            int[] v = new int[5];
+            bool trocou;
+
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Escreva um número: ");
+                v[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Original: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+
+            do
+            {
+                trocou = false;
+                for(int i = 0; i < 4; i++)
+                {
+                    if(v[i] > v[i + 1])
+                    {
+                        temp = v[i];
+                        v[i] = v[i + 1];
+                        v[i + 1] = temp;
+                        trocou = true;
+                    }
+                }
+            } while (trocou);
+
+            Console.WriteLine("\nOrdenado: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+        }
+    }
+}
---
+++ DAD/08.03/selectionSort/selectionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace selectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            int[] v = new int[5];
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"Write number in pos {i}: ");
+                v[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Original: ");
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+
+            for(int i = 0; i < 4; i++)
+            {
+                min = i;
+
+                for(int j = (i + 1); j < 5; j++)
+                {
+                    if(v[j] < v[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if(i != min)
+                {
+                    int temp = v[i];
+                    v[i] = v[min];
+                    v[min] = temp;
+                }
+            }
+
+            Console.WriteLine("\nSorted: ");
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+        }
+    }
+}
