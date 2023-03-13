59fdb7b5ab56b634b3d51a409ca5248ea306118d
Jbernardiss <joaobernardiss@gmail.com>
2023-03-12 22:41:30 -0300

    Adding DAD sorting algo. exercises

---
+++ DAD/08.03/Ex1/Ex1/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
+
+namespace A
+{
+    internal class Program
+    {
+
+        static int[] bubbleSort(int[] numArr)
+        {
+            bool changed;
+            int temp;
+
+            do
+            {
+                changed = false;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (numArr[i] > numArr[i + 1])
+                    {
+                        temp = numArr[i];
+                        numArr[i] = numArr[i + 1];
+                        numArr[i + 1] = temp;
+                        changed = true;
+                    }
+                }
+            } while (changed);
+
+            return numArr;
+        }
+
+        static int[] selectionSort(int[] numArr)
+        {
+            int temp;
+            int min;
+
+            for (int i = 0; i < 10; i++)
+            {
+                min = i;
+
+                for(int j = (i + 1); j < 10; j++)
+                {
+                    if (numArr[j] < numArr[min])
+                    {
+                        min = j;
+                    }
+
+                    if(min != i)
+                    {
+                        temp = numArr[i];
+                        numArr[i] = temp;
+                        numArr[min] = temp;
+                    }
+                }
+            }
+
+            return numArr;
+        }
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numArr = new int[10];
+            int[] increasingNumArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] decreasingNumArr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            int[] bubbleSorted, selectionSorted, bubbleSortedIncreasing, bubbleSortedDecreasing, selectionSortedIncreasing, selectionSortedDecreasing;
+            
+
+            for(int i = 0; i < 10; i++)
+            {
+                numArr[i] = rnd.Next(0, 101);
+            }
+
+            Console.WriteLine("Original:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{numArr[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Increasing Numbers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{increasingNumArr[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Decreasing Numbers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{decreasingNumArr[i]}, ");
+            }
+            Console.WriteLine();
+
+            bubbleSorted = bubbleSort(numArr);
+            bubbleSortedIncreasing = bubbleSort(increasingNumArr);
+            bubbleSortedDecreasing = bubbleSort(decreasingNumArr);
+            selectionSorted = selectionSort(numArr);
+            selectionSortedIncreasing = selectionSort(increasingNumArr);
+            selectionSortedDecreasing = selectionSort(decreasingNumArr);
+
+            Console.WriteLine("Random Bubble Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{bubbleSorted[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Crescent Bubble Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{bubbleSortedIncreasing[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Decrescent Bubble Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{bubbleSortedDecreasing[i]}, ");
+            }
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Random Selection Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{selectionSorted[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Increasing Selection Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{selectionSortedIncreasing[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Decreasing Bubble Sorted:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{selectionSortedDecreasing[i]}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DAD/08.03/Ex2/Ex2/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Ex2
+{
+    internal class Program
+    {
+        static int[] bubbleSort(int[] numArr)
+        {
+            bool changed;
+            int temp;
+
+            do
+            {
+                changed = false;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (numArr[i] > numArr[i + 1])
+                    {
+                        temp = numArr[i];
+                        numArr[i] = numArr[i + 1];
+                        numArr[i + 1] = temp;
+                        changed = true;
+                    }
+                }
+            } while (changed);
+
+            return numArr;
+        }
+
+        static int[] selectionSort(int[] numArr)
+        {
+            int temp;
+            int min;
+
+            for (int i = 0; i < 10; i++)
+            {
+                min = i;
+
+                for (int j = (i + 1); j < 10; j++)
+                {
+                    if (numArr[j] < numArr[min])
+                    {
+                        min = j;
+                    }
+
+                    if (min != i)
+                    {
+                        temp = numArr[i];
+                        numArr[i] = temp;
+                        numArr[min] = temp;
+                    }
+                }
+            }
+
+            return numArr;
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr1K = new int[1000];
+            //int[] arr10K = new int[10000];
+            //int[] arr100K = new int[100000];
+
+            int[] arrSorted1K = new int[1000];
+            //int[] arrSorted10K = new int[10000];
+            //int[] arrSorted100K = new int[100000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                arr1K[i] = 1000 - i;
+            }
+
+            /* for (int i = 0; i < 10000; i++)
+            {
+                arr10K[i] = rand.Next(0, 101);
+            }
+            for (int i = 0; i < 100000; i++)
+            {
+                arr100K[i] = rand.Next(0, 101);
+            } */
+
+            arrSorted1K = bubbleSort(arr1K);
+            /* arrSorted10K = selectionSort(arr10K);
+            arrSorted100K = selectionSort(arr100K); */
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"{arr1K[i]}, ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"{arrSorted1K[i]}, ");
+            }
+        }
+    }
+}
