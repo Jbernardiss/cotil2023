10f75719ab3f93ff5eaf6bce8addadff49bb3bab
Jbernardiss <joaobernardiss@gmail.com>
2023-03-15 15:22:45 -0300

    bruh

---
+++ DAD/15.03/LetterSort/LetterSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LetterSort
+{
+    class Program
+    {
+        static string letterSort(string word)
+        {
+            char[] charArr = word.ToCharArray();
+            bool changed = false;
+            char temp;
+
+            do
+            {
+                for(int i = 0; i < (word.Length - 1); i++)
+                {
+                    if(charArr[i] > charArr[i + 1])
+                    {
+                        temp = charArr[i];
+                        charArr[i] = charArr[i + 1];
+                        charArr[i + 1] = temp;
+
+                        changed = true;
+                        char[] bruh = charArr;
+                    }
+                }
+            } while (changed);
+
+            return new string(charArr);
+        } 
+
+        static void Main(string[] args)
+        {
+            string name = "Priscila";
+
+            name = letterSort(name);
+
+            Console.WriteLine(name);
+        }
+    }
+}
